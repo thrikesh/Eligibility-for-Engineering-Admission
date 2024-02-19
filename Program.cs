523f179d8e02da541718caa17f5cd376722b6950
thrikeswar <119576222+thrikesh@users.noreply.github.com>
2024-02-19 09:50:24 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,31 @@
-﻿using System;
-    class Eligibility
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class eligiblity
+{
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+      int maths,physics,chemistry,totalmark,sum;
+        Console.WriteLine("enter the physics mark");
+        physics=int.Parse(Console.ReadLine());
+        Console.WriteLine("enter the maths mark");
+        maths=int.Parse(Console.ReadLine());
+        Console.WriteLine("enter the chemistry mark");
+        chemistry=int.Parse(Console.ReadLine());
+        totalmark=maths+physics+chemistry;
+        sum=maths+physics;
+        if(chemistry>=50 && maths>=65 && physics>=55)
         {
-           Console.WriteLine("Hiiiiiii");
+            if(totalmark>=180 || sum>=140)
+            {
+                Console.WriteLine("the candidate is eligible for admission.");
+            }
+            else
+            {
+                Console.WriteLine("the candidate is not eligible for admission.");
+            }
         }
     }
-
+}
